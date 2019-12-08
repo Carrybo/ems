d6080678300f083fe1dfce201bf82fd302ce6de6
Nts <ning1996tianshu@qq.com>
2019-12-08 14:10:45 +0800

    ems添加员工功能测试通过

--- src/test/java/com/nts/TestService.java
+++ src/test/java/com/nts/TestService.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @SpringBootTest(classes = EmsGitApplication.class)
 @RunWith(SpringRunner.class)
@@ -20,5 +22,12 @@ public class TestService {
     public void test0() {
         List<Emp> emps = es.queryAll(1, 5);
         emps.forEach(emp -> System.out.println(emp));
+    }
+
+    @Test
+    public void test1() {
+        Emp emp = new Emp();
+        emp.setId(UUID.randomUUID().toString()).setAge(18).setBir(new Date()).setName("git添加").setSalary(500.0);
+        es.addEmp(emp);
     }
 }
